b1ca8423896e70ca53d3de3b4b829fb5014ab380
Jan Schulte <hello@unexpected-co.de>
2016-08-23 21:37:22 +0200

    Add webpack as watcher

--- config/dev.exs
+++ config/dev.exs
@@ -11,7 +11,9 @@ config :datjournaal, Datjournaal.Endpoint,
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
-  watchers: []
+  watchers: [
+    node: ["node_modules/webpack/bin/webpack.js", "--watch", "--color"]
+  ]
 
 
 # Watch static and templates for browser reloading.
