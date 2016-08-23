17f561b87a63da208fd42845b78a1faab10a9281
Jan Schulte <hello@unexpected-co.de>
2016-08-23 21:38:52 +0200

    Render main page

--- web/router.ex
+++ web/router.ex
@@ -16,7 +16,7 @@ defmodule Datjournaal.Router do
   scope "/", Datjournaal do
     pipe_through :browser # Use the default browser stack
 
-    get "/", PageController, :index
+    get "*path", PageController, :index
   end
 
   # Other scopes may use custom stacks.
