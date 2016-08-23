88e51abb2018ad22b88f5ca677a103b6fd6f2a39
Jan Schulte <hello@unexpected-co.de>
2016-08-23 22:24:20 +0200

    Validate and hash passwords for user

--- web/models/user.ex
+++ web/models/user.ex
@@ -4,17 +4,34 @@ defmodule Datjournaal.User do
   schema "users" do
     field :handle, :string
     field :email, :string
+    field :password, :string, virtual: true
     field :encrypted_password, :string
 
     timestamps()
   end
+
+  @required_fields ~w(handle email password)
+  @optional_fields ~w(encrypted_password)
 
   @doc """
   Builds a changeset based on the `struct` and `params`.
   """
   def changeset(struct, params \\ %{}) do
     struct
-    |> cast(params, [:handle, :email, :encrypted_password])
-    |> validate_required([:handle, :email, :encrypted_password])
+    |> cast(params, @required_fields, @optional_fields)
+    |> validate_format(:email, ~r/@/)
+    |> validate_length(:password, min: 5)
+    |> validate_confirmation(:password, message: "Password does not match")
+    |> unique_constraint(:email, message: "Email already taken")
+    |> generate_encrypted_password
+  end
+
+  defp generate_encrypted_password(current_changeset) do
+    case current_changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: password}} ->
+        put_change(current_changeset, :encrypted_password, Comeonin.Bcrypt.hashpwsalt(password))
+      _ ->
+        current_changeset
+    end
   end
 end
