ee9131893ef63c81c219b2db6869fc27546929db
Jan Schulte <jan.schulte@asquera.de>
2016-09-01 17:58:11 +0200

    Add whitelist for allowed uploadable file types

--- web/uploaders/image.ex
+++ web/uploaders/image.ex
@@ -10,9 +10,9 @@ defmodule Datjournaal.Image do
   # @versions [:original, :thumb]
 
   # Whitelist file extensions:
-  # def validate({file, _}) do
-  #   ~w(.jpg .jpeg .gif .png) |> Enum.member?(Path.extname(file.file_name))
-  # end
+  def validate({file, _}) do
+    ~w(.jpg .jpeg .gif .png) |> Enum.member?(Path.extname(file.file_name))
+  end
 
   # Define a thumbnail transformation:
   # def transform(:thumb, _) do
