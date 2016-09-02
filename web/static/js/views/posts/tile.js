f9b6aaf14fd8bf5f12742dde0335b581f330c072
Jan Schulte <hello@unexpected-co.de>
2016-09-02 22:28:41 +0200

    Use moment.js to render human readable dates

--- web/static/js/views/posts/tile.js
+++ web/static/js/views/posts/tile.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import moment from 'moment';
 
 export class TileComponent extends React.Component {
   render() {
     let { description, imageUrl, postedAt } = this.props;
+
+    let date = moment(postedAt).calendar();
 
     return (
       <div className="row tile">
         <div className="col-xs-12">
           <img className="image" src={imageUrl} />
           <div>description: {description}</div>
-          <div>posted at: {postedAt}</div>
+          <div>posted at: {date}</div>
         </div>
       </div>
     );
