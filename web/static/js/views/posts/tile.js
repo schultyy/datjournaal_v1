c1c27df105f5a8ec100fef0ea0e476db1c6ec692
Jan Schulte <hello@unexpected-co.de>
2016-09-02 22:24:04 +0200

    Render posted at date for every picture

--- web/static/js/views/posts/tile.js
+++ web/static/js/views/posts/tile.js
@@ -2,13 +2,14 @@ import React from 'react';
 
 export class TileComponent extends React.Component {
   render() {
-    let { description, imageUrl } = this.props;
+    let { description, imageUrl, postedAt } = this.props;
 
     return (
       <div className="row tile">
         <div className="col-xs-12">
           <img className="image" src={imageUrl} />
           <div>description: {description}</div>
+          <div>posted at: {postedAt}</div>
         </div>
       </div>
     );
