8735d7e6051e1dc06b56c1145e7df6afc472f6e6
Jan Schulte <jan.schulte@asquera.de>
2016-09-13 18:03:54 +0200

    Pass post object directly to tile
    Before this was not possible because the image url was calculated
    outside depending on whether the post was shown in the home view or in
    detail. When it was shown in detail the enclosing component passed in
    the appropriate image url

--- web/static/js/views/posts/tile.js
+++ web/static/js/views/posts/tile.js
@@ -5,22 +5,18 @@ import {absoluteUrlForPost} from '../../utils';
 
 export class TileComponent extends React.Component {
   onCopyURLToClipboard() {
-    const { id } = this.props;
-    return absoluteUrlForPost(id);
+    const { post } = this.props;
+    return absoluteUrlForPost(post.id);
   }
 
   render() {
-    let {
-      id,
-      description,
-      imageUrl,
-      postedAt,
-      userHandle,
+    const {
+      post,
       onDoubleClick,
       isDetailMode,
     } = this.props;
 
-    let date = moment(postedAt).calendar();
+    let date = moment(post.inserted_at).calendar();
 
     if (isDetailMode) {
       var doubleClickHandler = null;
@@ -32,16 +28,16 @@ export class TileComponent extends React.Component {
     return (
       <div className="tile" onDoubleClick={onDoubleClick}>
         <div>
-          <span className="pull-left">{userHandle}</span>
+          <span className="pull-left">{post.user.handle}</span>
           <span className="pull-right">
             <ContextMenu
               onCopyURLToClipboard={this.onCopyURLToClipboard.bind(this)}
               onShowDetail={doubleClickHandler} />
           </span>
         </div>
-        <img src={imageUrl} />
+        <img src={post.image} />
         <div className="date">{date}</div>
-        <div className="description">{description}</div>
+        <div className="description">{post.description}</div>
       </div>
     );
   }
