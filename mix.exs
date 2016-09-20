1e065352488155e0470400167f830a16b224df08
Jan Schulte <jan.schulte@asquera.de>
2016-09-20 18:50:02 +0200

    add all the apps

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule Datjournaal.Mixfile do
   def application do
     [mod: {Datjournaal, []},
      applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :postgrex, :arc_ecto]]
+                    :phoenix_ecto, :postgrex, :arc_ecto, :guardian, :arc, :comeonin]]
   end
 
   # Specifies which paths to compile per environment.
