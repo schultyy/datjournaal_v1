04190b461ece43abb333ba239f03ec00631a5d4a
Jan Schulte <hello@unexpected-co.de>
2016-10-17 23:20:37 +0200

    Set twitter meta tags for post detail
    This happens on the server when the client asks for
    https://datjournaal.de/<slug>. This needs to be resetted by JS on the
    client side when the user switches back to the home page

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,7 +1,19 @@
 defmodule Datjournaal.PageController do
   use Datjournaal.Web, :controller
 
-  def index(conn, _params) do
-    render conn, "index.html"
+  alias Datjournaal.{Repo, Post}
+
+  def index(conn, params) do
+    render_index_with_params(conn, Map.get(params, "path"))
+  end
+
+  defp render_index_with_params(conn, []) do
+    render conn, "index.html", post: nil
+  end
+
+  defp render_index_with_params(conn, params) do
+    slug = params |> List.first
+    post = Repo.get_by(Post, slug: slug)
+    render conn, "index.html", post: post
   end
 end
