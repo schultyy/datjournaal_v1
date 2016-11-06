e162ee73fe4074622cc34fd5b3dff6861182f3ce
Jan Schulte <hello@unexpected-co.de>
2016-11-06 12:36:10 +0100

    Render tweet /w camera emoji
    This is a hint for people that the included link is pointing to a
    picture, not just text

--- web/models/tweet.ex
+++ web/models/tweet.ex
@@ -4,14 +4,22 @@ defmodule Datjournaal.Tweet do
   end
 
   def to_tweet(url, original_text) do
-    #We substract four at the end to have enough space for three dots and a line break
-    max_text_length = 140 - String.length(url) - 4
+    max_text_length = 140 - String.length(url) - dotlength - String.length(prefix)
     if String.length(original_text) > max_text_length do
       text = String.slice(original_text, 0, max_text_length) <> "..."
     else
       text = original_text
     end
 
-    Enum.join([text, url], "\n")
+    Enum.join([prefix <> text, url], "\n")
+  end
+
+  defp dotlength do
+    #Dot length is four at the end to have enough space for three dots and a line break
+    4
+  end
+
+  defp prefix do
+    "📸 "
   end
 end
