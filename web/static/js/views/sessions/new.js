87ec347853fca3e19e94b72e6b946585eda352cf
Jan Schulte <hello@unexpected-co.de>
2016-11-17 12:31:54 +0100

    Improve layout of login page

--- web/static/js/views/sessions/new.js
+++ web/static/js/views/sessions/new.js
@@ -33,22 +33,25 @@ class SessionsNew extends React.Component {
 
   render() {
     return (
-      <div className='view-container sessions new'>
-        <main>
-          <header>
-            <div className="logo" />
-          </header>
-          <form onSubmit={::this._handleSubmit}>
-            {::this._renderError()}
-            <div className="field">
-              <input ref="email" type="Email" placeholder="Email" required="true" />
-            </div>
-            <div className="field">
-              <input ref="password" type="password" placeholder="Password" required="true" />
-            </div>
-            <button type="submit">Sign in</button>
-          </form>
-        </main>
+      <div>
+        <div className="row">
+          <main className="col-xs-12 col-md-offset-4 col-md-4 container login">
+            <header>
+              <div className="logo">dat Journaal</div>
+              <h3>Login</h3>
+            </header>
+            <form onSubmit={::this._handleSubmit}>
+              {::this._renderError()}
+              <div className="field">
+                <input ref="email" type="Email" placeholder="Email" required="true" />
+              </div>
+              <div className="field">
+                <input ref="password" type="password" placeholder="Password" required="true" />
+              </div>
+              <button type="submit">Sign in</button>
+            </form>
+          </main>
+        </div>
       </div>
     );
   }
