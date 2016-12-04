e3a53ead9a39713669f5e3b3d28c052e86b8c0f7
Jan Schulte <hello@unexpected-co.de>
2016-12-04 15:28:25 +0100

    Add basic layout for all password controls

--- web/static/js/views/userdetails/index.js
+++ web/static/js/views/userdetails/index.js
@@ -7,6 +7,17 @@ export default class UserDetails extends React.Component {
         <div className="row">
           <div className="col-xs-12">
             <h1>Change password</h1>
+            <form>
+              <div className="form-group">
+                <label htmlFor="currentPassword">Current password</label>
+                <input className="form-control" type="text" name="currentPassword" />
+              </div>
+              <div className="form-group">
+                <label htmlFor="newPassword">New password</label>
+                <input className="form-control" type="text" name="newPassword" />
+              </div>
+              <button className="form-control">Set password</button>
+            </form>
           </div>
         </div>
       </div>
