1bb8522f0fed673ac7ad6adaba7ac637ddf04220
Jan Schulte <hello@unexpected-co.de>
2016-12-04 15:23:52 +0100

    add userdetails css class

--- web/static/js/views/userdetails/index.js
+++ web/static/js/views/userdetails/index.js
@@ -3,9 +3,11 @@ import React from 'react';
 export default class UserDetails extends React.Component {
   render() {
     return (
-      <div className="row">
-        <div className="col-xs-12">
-          <h1>Change password</h1>
+      <div className="userdetails">
+        <div className="row">
+          <div className="col-xs-12">
+            <h1>Change password</h1>
+          </div>
         </div>
       </div>
     );
