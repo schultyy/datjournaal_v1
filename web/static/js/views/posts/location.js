fb8beb6d349360f7b984723debca0a9ef86f13e0
Jan Schulte <hello@unexpected-co.de>
2017-03-07 21:34:06 +0100

    Redo location form

--- web/static/js/views/posts/location.js
+++ web/static/js/views/posts/location.js
@@ -33,18 +33,15 @@ export default class PickLocation extends React.Component {
 
     return (
       <div className="location-form">
-        <div className="col-xs-8 col-md-8">
-          <input ref="locationname" className="location-input form-control" type="text" name="location" />
+        <div className="location-input">
+          <input ref="locationname" type="text" name="location" />
         </div>
-        <div className="col-xs-4 col-md-2">
-          <button onClick={this.onQueryLocationClick.bind(this)} className="btn btn-default search-button">Search</button>
+        <div>
+          <button onClick={this.onQueryLocationClick.bind(this)} className="btn search-button">Search</button>
         </div>
-        <div className="clearfix" />
-        <div className="col-xs-12">
-          <ul>
-            {this.renderSearchResults(locations)}
-          </ul>
-        </div>
+        <ul>
+          {this.renderSearchResults(locations)}
+        </ul>
       </div>
     );
   }
